0a81db0864309e6e552f32ad0aa5bd689e3200b3
Deivid Augusto Silva <agunegocios@hotmail.com>
2024-06-06 16:25:21 -0300

    03 - Exercicio 3 Estruturas sequenciais

---
+++ Exercicios/Estruturas-Sequenciasis/ex03.py
@@ -0,0 +1,9 @@
+"""
+Faça um Programa que peça dois números e imprima a soma.
+"""
+
+num_1 = int(input('Digite o primeiro número: '))
+num_2 = int(input('Digite o segundo número: '))
+soma = num_1 + num_2
+
+print(f'O resulta da soma entre {num_1} + {num_2} = {soma}.')
