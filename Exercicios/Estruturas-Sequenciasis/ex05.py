1bcba94b33ef51215a50a4faee73aaecdcc4f1ad
Deivid Augusto Silva <agunegocios@hotmail.com>
2024-06-10 12:21:44 -0300

    05 - Exercicio 5 Estruturas sequenciais

---
+++ Exercicios/Estruturas-Sequenciasis/ex05.py
@@ -0,0 +1,12 @@
+"""
+Faça um Programa que converta metros para centímetros.
+"""
+
+print('-'*10)
+print('CONVERSOS DE MEDIDAS')
+print('-'*10)
+
+medida_m = float(input('Informe a medida em metros que deseja converter para centímetros: '))
+medida_cm = medida_m * 100
+
+print(f'{medida_m} m em centímetros é {medida_cm} cm.')
