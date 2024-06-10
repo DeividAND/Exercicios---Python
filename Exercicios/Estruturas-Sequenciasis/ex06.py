ba8c2d9c29e06ce27761d1fd5d9295f3d2ab2eaf
Deivid Augusto Silva <agunegocios@hotmail.com>
2024-06-10 12:23:25 -0300

    06 - Exercicio 6 Estruturas sequenciais

---
+++ Exercicios/Estruturas-Sequenciasis/ex06.py
@@ -0,0 +1,8 @@
+"""
+Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+"""
+
+raio = float(input('Informe o raio do círculo que deseja calcular a área: '))
+area_circulo = 3.14 * (raio ** 2)
+
+print(f'A área do circulo é {area_circulo}.')
