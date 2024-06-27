1d0c89cec256edfdee302e3939b6423ebcb7c867
Deivid Augusto Silva <agunegocios@hotmail.com>
2024-06-26 22:03:39 -0300

    11 - Exercicio 11 Estruturas sequenciais

---
+++ Exercicios/Estruturas-Sequenciasis/ex11.py
@@ -0,0 +1,9 @@
+"""
+Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+o produto do dobro do primeiro com metade do segundo .
+a soma do triplo do primeiro com o terceiro.
+o terceiro elevado ao cubo.
+"""
+num_1 = int(input('Informe o primeiro número: '))
+num_2 = int(input('Informe o segundo número: '))
+num_real = float(input('Informe um número real : '))
